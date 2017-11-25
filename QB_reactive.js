89ef34b8d193936aba2ed4d9160214ec3ec290dc
Sn-Kinos <dony435@naver.com>
2017-11-25 16:27:06 +0900

    comcom

--- QB_reactive.js
+++ QB_reactive.js
@@ -9,6 +9,18 @@ if( navigator.userAgent.indexOf('Firefox') >= 0 ) {
 		}, true );
 	}
 }
+
+skill_flag = 0
+function menu_skill_click(argument) {
+	if (!skill_flag) {
+		document.getElementById('list_skill').setAttribute('style', 'display: block;');
+		skill_flag = 1
+	}
+	else{
+		document.getElementById('list_skill').setAttribute('style', 'display: none;');
+		skill_flag = 0
+	}
+}
 function getJson(url) {
 	var httpReq = new XMLHttpRequest();
 	httpReq.open("GET", url, false);
