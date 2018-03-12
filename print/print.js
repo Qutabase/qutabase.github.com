9826d97cb76bf4451d7bba813fd22a6842548b6b
Sn-Kinos <dony435@naver.com>
2018-03-13 00:29:47 +0900

    QUTABASE

--- print/print.js
+++ print/print.js
@@ -2,88 +2,27 @@
 	KODEX PRINT SIMULATOR
 	2018-03-10
 	DEVELOPED BY SN KINOS
-
-	PRINT TABLE
-		SEASON	|	NOT		|	SUM
-	SSR	1.0%	|	1.5%	|	2.5%
-	SR+	1.5%	|	1.5%	|	3.0%
-	SR	5.0%	|	10.0%	|	15.0%
-	R+	10.0%	|	30.0%	|	40.0%
-	R			39.5%		|	39.5%
-
-	11 BONUS
-		SEASON	|	NOT		|	SUM
-	SSR	3.4%	|	1.6%	|	5.0%
-	SR+	40.0%	|	55.0%	|	95.0%
-
-	PREMIUM MILEAGE
-		SEASON	|	NOT		|	SUM
-	SSR	22.0%	|	11.0%	|	33.0%
-	SR+	40.0%	|	27.0%	|	67.0%
-
-	SSR PREMIUM MILEAGE
-		SEASON	|	NOT		|	SUM
-	SSR	66.6%	|	33.4%	|	100.0%
 */
 
 printCheck = {
 
 }
-function print() {
-	var	list	=	document.getElementById('print_list');
-	
-	for (var i = 0; i < 11; i++) {
 
-		var	rand	=	Math.floor(
-							Math.random()	*	1000
-						) + 1;
-		var	range	=	[0,0];
+var	list	=	document.getElementById('print_list');
+var	res		=	document.getElementById('print_result');
 
-		if	(rand < 10) {
-			//	SEASON SSR
-			range[1]	=	eng.season.SSR[1];
-			range[0]	=	eng.season.SSR[0];
-		}
-		else if (rand < 25) {
-			//	Non-SEASON SSR
-			range[1]	=	eng.season.SSR[0];
-			range[0]	=	17001;
-		}
-		else if (rand < 40) {
-			//	SEASON SR
-			range[1]	=	eng.season['SR+'][1];
-			range[0]	=	eng.season['SR+'][0];
-		}
-		else if (rand < 55){
-			//	Non-SEASON SR
-			range[1]	=	eng.season['SR+'][0];
-			range[0]	=	6001;
-		}
-		else if(rand < 105) {
-			//	SEASON SR
-			range[1]	=	eng.season.SR[1];
-			range[0]	=	eng.season.SR[0];
-		}
-		else if(rand < 205) {
-			//	Non-SEASON SR
-			range[1]	=	eng.season.SR[0];
-			range[0]	=	5001;
-		}
-		else if	(rand < 305) {
-			//	SEASON R+
-			range[1]	=	eng.season['R+'][1];
-			range[0]	=	eng.season['R+'][0];
-		}
-		else if	(rand < 605) {
-			//	Non-SEASON R+
-			range[1]	=	1;
-		}
-		else {
-			//	R
-			range[0]	=	1;
-		}
+eng.season.SR[1]	-=	3;	//	EXCEPTING MONSTER KODEX
 
-		var	pri_id	=	range[0]
+/*	//	ADD / ON HEAD OF THIS LINE
+eng.season.SSR[1]		-=	2;	//	EPISODE 1
+eng.season['SR+'][1]	-= 2;
+/*/
+eng.season.SSR[0]		+=	2;	//	EPISODE 2
+eng.season['SR+'][0]	+=	2;
+//*/
+
+function result(range) {
+	var	pri_id		=	range[0]
 						+	Math.floor(
 								Math.random()
 								*	(
@@ -91,22 +30,250 @@ function print() {
 										+	1
 									)
 							);
-		if (pri_id > 2) {
 
-		}
-		else {
+	var	key_list	=	Object.keys(Jdex);
+	var	dex			=	{};
+	var	kodexLink	=	'#';
+	var	imgLink		=	"../raw.png";
+	var	bgColor		=	'';
+	if (pri_id > 2) {
+		for (var j = 0; j < key_list.length; j++) {
+			if (Jdex[key_list[j]].id == pri_id) {
 
-			if (range[1] == 1) {
-				pri_id	=	1;
+				dex			=	Jdex[key_list[j]];
+				imgLink		=	'https://raw.githubusercontent.com/Sn-Kinos/Qutabase/master/Kodex/'
+								+	role[dex.role]		+	'/'
+								+	dex.enskill			+	'/'
+								+	rarity[dex.rarity]	+	'/'
+								+	dex.id				+	'/raw.png'
+								;
+				kodexLink	=	'../index.html?kodexName='
+								+	encodeURIComponent(dex.name)
+								;
+				bgColor		=	dex.rareColor;
+
 			}
-			else {
-				pri_id	=	0;
-			}
-
 		}
+	}
+	else {
 
-		
+		imgLink	=	range[0]	+	'.png';
+		pri_id	=	range[0];
+		bgColor	=	(range[0] == 2)
+					?
+					"#FFE746"
+					:
+					"#B6D8F5"
+					;
 
 	}
+
+	if (printCheck[pri_id]) {
+		printCheck[pri_id]	+=	1;
+		var	elem			=	document.getElementById('res_' + pri_id);
+		elem.innerHTML		=	'<div><img class="res_img" src="'
+								+	imgLink		+	'"></div><br>x'
+								+	printCheck[pri_id]
+								;
+	}
+	else {
+		printCheck[pri_id]	=	1;
+		var	elem			=	document.createElement('div');
+		elem.setAttribute(	'id',		'res_'	+	pri_id);
+		elem.setAttribute(	'class',	'res_kodex');
+		elem.style.background	=	bgColor;
+		elem.setAttribute(	'onclick'
+				,	'location.href="'
+					+	kodexLink
+					+	'";'
+		);
+		elem.innerHTML		=	'<div><img class="res_img" src="'
+								+	imgLink		+	'"></div><br>x'
+								+	printCheck[pri_id]
+								;
+		list.appendChild(elem)
+	}
+}
+
+/*
+	print1()	-	PREMIUM PRINT
+		SEASON	|	NOT		|	SUM
+	SSR	1.0%	|	1.5%	|	2.5%
+	SR+	1.5%	|	1.5%	|	3.0%
+	SR	5.0%	|	10.0%	|	15.0%
+	R+	10.0%	|	30.0%	|	40.0%
+	R			39.5%		|	39.5%
+*/
+
+function print1(argument) {
+
+	var	rand	=	Math.floor(
+						Math.random()	*	1000
+					);
+	var	range	=	[0,0];
+
+	if	(rand < 10) {
+		//	SEASON SSR
+		range[1]	=	eng.season.SSR[1];
+		range[0]	=	eng.season.SSR[0];
+	}
+	else if (rand < 25) {
+		//	Non-SEASON SSR
+		range[1]	=	eng.season.SSR[0];
+		range[0]	=	17001;
+	}
+	else if (rand < 40) {
+		//	SEASON SR+
+		range[1]	=	eng.season['SR+'][1];
+		range[0]	=	eng.season['SR+'][0];
+	}
+	else if (rand < 55){
+		//	Non-SEASON SR+
+		range[1]	=	eng.season['SR+'][0];
+		range[0]	=	6001;
+	}
+	else if(rand < 105) {
+		//	SEASON SR
+		range[1]	=	eng.season.SR[1];
+		range[0]	=	eng.season.SR[0];
+	}
+	else if(rand < 205) {
+		//	Non-SEASON SR
+		range[0]	=	2
+	}
+	else if	(rand < 305) {
+		//	SEASON R+
+		range[1]	=	eng.season['R+'][1];
+		range[0]	=	eng.season['R+'][0];
+	}
+	else if	(rand < 605) {
+		//	Non-SEASON R+
+		range[0]	=	1;
+	}
+	else {
+		//	R
+		;
+	}
+
+	result(range);
+
+}
+
+/*
+	printB()	-	11 BONUS
+		SEASON	|	NOT		|	SUM
+	SSR	3.4%	|	1.6%	|	5.0%
+	SR+	40.0%	|	55.0%	|	95.0%
+*/
+
+function printB(argument) {
+
+	var	rand	=	Math.floor(
+						Math.random()	*	1000
+					);
+	var	range	=	[0,0];
+
+	if (rand < 34) {
+		//	SEASON SSR
+		range[1]	=	eng.season.SSR[1];
+		range[0]	=	eng.season.SSR[0];
+
+	}
+	else if (rand < 50) {
+		//	Non-SEASON SSR
+		range[1]	=	eng.season.SSR[0];
+		range[0]	=	17001;
+	}
+	else if (rand < 450) {
+		//	SEASON SR+
+		range[1]	=	eng.season['SR+'][1];
+		range[0]	=	eng.season['SR+'][0];
+	}
+	else {
+		//	Non-SEASON SR+
+		range[1]	=	eng.season['SR+'][0];
+		range[0]	=	6001;
+	}
+
+	result(range);
+
+}
+
+function print11() {
+
+	document.getElementById('print_cash').innerHTML	=	Number(document.getElementById('print_cash').innerHTML) + 50;
+	
+	for (var i = 0; i < 10; i++)
+		print1();
+	printB();
+
+}
+
+/*
+	printm()	-	PREMIUM MILEAGE
+		SEASON	|	NOT		|	SUM
+	SSR	22.0%	|	11.0%	|	33.0%
+	SR+	40.0%	|	27.0%	|	67.0%
+*/
+
+function printm() {
+
+	var	rand	=	Math.floor(
+						Math.random()	*	100
+					);
+	var	range	=	[0,0];
+
+	if (rand < 22) {
+		//	SEASON SSR
+		range[1]	=	eng.season.SSR[1];
+		range[0]	=	eng.season.SSR[0];
+
+	}
+	else if (rand < 33) {
+		//	Non-SEASON SSR
+		range[1]	=	eng.season.SSR[0];
+		range[0]	=	17001;
+	}
+	else if (rand < 73) {
+		//	SEASON SR+
+		range[1]	=	eng.season['SR+'][1];
+		range[0]	=	eng.season['SR+'][0];
+	}
+	else {
+		//	Non-SEASON SR+
+		range[1]	=	eng.season['SR+'][0];
+		range[0]	=	6001;
+	}
+
+	result(range);
+
+}
+
+/*
+	printM()	-	SSR PREMIUM MILEAGE
+		SEASON	|	NOT		|	SUM
+	SSR	66.6%	|	33.4%	|	100.0%
+*/
+
+function printM() {
+
+	var	rand	=	Math.floor(
+						Math.random()	*	1000
+					);
+	var	range	=	[0,0];
+
+	if (rand < 666) {
+		//	SEASON SSR
+		range[1]	=	eng.season.SSR[1];
+		range[0]	=	eng.season.SSR[0];
+
+	}
+	else {
+		//	Non-SEASON SSR
+		range[1]	=	eng.season.SSR[0];
+		range[0]	=	17001;
+	}
+
+	result(range);
 
 }
