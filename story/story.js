40efdca48391148f273dac91c34f3eaaef3f7695
Sn Kinos <dony435@naver.com>
2018-05-09 00:53:42 +0900

    QUTABASE

--- story/story.js
+++ story/story.js
@@ -1,6 +1,8 @@
 function dialogPrs(argument) {
-	dialogs	=	argument.split('#');
+	var	dialogs	=	argument.split('#');
 	console.log(mainDlg);
 }
 
-mainStory	=	JSON.parse(getJson("https://raw.githubusercontent.com/Sn-Kinos/Qutabase/master/qurare.json"));
+mainStory	=	JSON.parse(getJson("https://raw.githubusercontent.com/Qutabase/qutabase.github.com/master/story/mainStory.json"));
+
+dialogPrs(mainStory);
